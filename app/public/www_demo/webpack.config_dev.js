5f3f0259e9e74e8aea6c0159e4c1902269dfc221
jackdizhu <376365334@qq.com>
2017-10-11 16:51:24 +0800

    webpack 配置

--- app/public/www_demo/webpack.config_dev.js
+++ app/public/www_demo/webpack.config_dev.js
@@ -1,8 +1,6 @@
 const webpack = require('webpack')
 const path = require('path')
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 var _path = './public/';
 
 module.exports = {
@@ -14,38 +12,26 @@ module.exports = {
         path: path.resolve(__dirname, './'),
         // 正常配置
         // publicPath: 'dist/',
-        // php 服务器配置
+        // php   服务器配置
         publicPath: '../dist/',
-        filename: 'js/[name].js',
+        filename: 'js/[name].js?v=[chunkhash:8]', // 每小块 单独的 hash 值
+        // filename: 'js/[name].js?' + new Date().getTime(), // 时间戳 版本号
         library: 'js/[name].js',
         libraryTarget: 'umd',
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                ],
+                test: /\.js(x)?$/,
+                loader: ['babel-loader'],
             },
             {
                 test: /\.css$/,
-                use: [
-                    {
-                        // loader: 'style-loader!css-loader?importLoaders=1&modules&localIdentName=[name]__[local]___[hash:base64:5]',
-                        loader: 'style-loader!css-loader',
-                    },
-                ],
+                loader: ['style-loader','css-loader']
             },
             {
                 test: /\.(jpe?g|png|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader?limit=8192&name=img/[name].[hash:8].[ext]',
-                    },
-                ],
+                loader: ['url-loader?limit=8192&name=./img/[name].[hash:8].[ext]'],
             },
             {
     　　　　　　test: /\.html$/,
@@ -58,5 +44,24 @@ module.exports = {
             }
         ],
     },
-    plugins: [new webpack.optimize.UglifyJsPlugin(), new webpack.optimize.ModuleConcatenationPlugin()]
+    resolve: {
+        alias: {
+            vue: path.resolve(__dirname,_path + 'dist/js/vue/vue.js'), //webpack打包时，需要设置别名
+            store: '../../store', //设置别名
+            img: path.resolve(__dirname,_path + 'src/img')
+        }
+    },
+    // 配置外部模块
+    // externals: {
+    //     vue: "window.Vue"
+    // }
+    plugins:[
+        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+            // filename: path.resolve(__dirname, _path + 'dist/html/demo08.html'), //生成的html存放路径，相对于 path
+            filename: '../html/demo08.html', //生成的html存放路径，相对于 path
+            template: path.resolve(__dirname, _path + 'src/html/demo08.html'), //html模板路径
+        }),
+        // new webpack.optimize.UglifyJsPlugin(),
+        // new webpack.optimize.ModuleConcatenationPlugin()
+    ]
 }
